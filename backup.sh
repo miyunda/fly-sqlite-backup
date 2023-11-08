f54771ca145fd53f446ee53e576842f9efeb2d34
miyunda <github@beijing2b.com>
2023-11-08 20:44:50 +0800

    change archive's filename

--- backup.sh
+++ backup.sh
@@ -12,7 +12,7 @@ BACKUP_DB="sqlite3 /data/db.sqlite3 '.backup /data/db.bak'"
 CREATE_ARCHIVE="gzip -cv /data/db.bak > /opt/$DATE.db.bak.gz"
 
 # Delete any archived files that are older than 30 days.
-CLEAN_UP="find /opt -type f -name 'db.bak.*.gz' -mtime +30 -exec rm '{}' +"
+CLEAN_UP="find /opt -type f -name '*.db.bak.gz' -mtime +30 -exec rm '{}' +"
 
 # Execute above of commands via a remote SSH console on your app.
 fly ssh console -a $APP_NAME -C 'bash -c "'"$INSTALL_SQLITE"' && '"$BACKUP_DB"' && '"$CREATE_ARCHIVE"' && '"$CLEAN_UP"'"'
