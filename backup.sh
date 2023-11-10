ae02e1550ced4551ddeabe0a6104118301769124
miyunda <github@beijing2b.com>
2023-11-10 22:45:38 +0800

    Initial commit

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 # Set the DATE variable to the current date from the runner VM, in the format YYYY-MM-DD.
 DATE=$(date +%F)
 
-# Install SQLite cli.
+# Install SQLite command-line interface.
 INSTALL_SQLITE="apt-get update && apt-get install sqlite3 -y"
 
 # Create a backup of the SQLite database.
@@ -12,9 +12,9 @@ BACKUP_DB="sqlite3 /data/db.sqlite3 '.backup /data/db.bak'"
 CREATE_ARCHIVE="gzip -cv /data/db.bak > /opt/$DATE.db.bak.gz"
 
 # Delete any archived files that are older than 30 days.
-CLEAN_UP="find /opt -type f -name '*.db.bak.gz' -mtime +30 -exec rm '{}' +"
+CLEAN_UP="find /opt -type f -name 'db.bak.*.gz' -mtime +30 -exec rm '{}' +"
 
-# Execute above of commands via a remote SSH console on your app.
+# Execute the above commands via a remote SSH console on your App.
 fly ssh console -a $APP_NAME -C 'bash -c "'"$INSTALL_SQLITE"' && '"$BACKUP_DB"' && '"$CREATE_ARCHIVE"' && '"$CLEAN_UP"'"'
 
 # Copy the created archive file to the runner machine.
